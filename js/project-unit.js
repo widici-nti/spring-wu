f9dba1518a7e5fef6ee0e79aa3a62a042b491c09
widici <84205124+widici@users.noreply.github.com>
2026-04-01 13:46:47 +0200

    Updated the design of the project cards and added checks for abscent url to the project unit web component

--- js/project-unit.js
+++ js/project-unit.js
@@ -6,7 +6,6 @@ class ProjectUnit extends HTMLElement {
         const title = this.getAttribute("title");
         const shortDesc = this.getAttribute("short");
         const longDesc = this.getAttribute("long") || shortDesc;
-        const banner = this.getAttribute("img") ? `<img id="banner" src="${this.getAttribute("img")}" alt="the ${title} banner">` : ``;
         const stack = (this.getAttribute("stack") || "").split(",").map(s => s.trim()).filter(Boolean).map(s => `<img src="img/icons/${s}.svg">`).join("\n");
 
         const stackIcons = `
@@ -15,16 +14,19 @@ class ProjectUnit extends HTMLElement {
             </div>
         `;
 
+        const header = url == null ? `<h3>${title}</h3>` :
+            `<a href=${url}>
+                <img src="img/icons/respository-accent.svg" alt="icon for the repository link">
+                <h3>${title}</h3>
+            </a>`
+
         this.innerHTML = `
-            ${banner}
             <div id="project-title">
-                <a href=${url}>
-                    <img src="img/icons/repository.svg" alt="icon for the repository link">
-                    <h3 id="title">${title}</h3>
-                </a>
-                <h3 id="pipe">|</h3>
-                <p id="short-desc">${shortDesc}</p>
-                <img src="img/icons/arrow.svg" alt="downwards arrow icon for expanding the project">
+                ${header}
+                <div id="short-desc">
+                    <p>${shortDesc}</p>
+                    <img src="img/icons/arrow.svg" alt="downwards arrow icon for expanding the project">
+                </div>
             </div>
             <p id="long-desc">${longDesc}</p>
             ${stackIcons}
