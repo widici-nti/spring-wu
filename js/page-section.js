19e11b3290a2be2c9c0133b92a4c827056a37145
widici <84205124+widici@users.noreply.github.com>
2026-04-12 14:57:25 +0200

    Added isolated shadow DOMs between page sections

--- js/page-section.js
+++ js/page-section.js
@@ -2,12 +2,14 @@ class PageSection extends HTMLElement {
     connectedCallback() {
         const file = this.getAttribute("src");
 
-        fetch(file)
-            .then(res => res.text())
-            .then(html => this.innerHTML = html);
-
         const id = file.split("/").pop().split(".")[0];
         this.setAttribute("id", id);
+
+        const shadow = this.attachShadow({ mode: "open" });
+
+        fetch(file)
+            .then(res => res.text())
+            .then(html => shadow.innerHTML = html);
     }
 }
 
